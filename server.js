38aaba0de5b1e5586cf8e5679af49ac84a49891d
lisagjh <lisagjhoorn@outlook.com>
2024-06-12 09:38:00 +0200

    picture and srcset added - webp and avif

--- server.js
+++ server.js
@@ -1,38 +1,46 @@
-// Import the Express.js module
 import express from "express";
-
-// Import the fetchJson function from a custom helper file
 import fetchJson from "./helpers/fetch-json.js";
 
-// Create a new Express app
 const app = express();
 
-// Set EJS as the template engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-// Serve static files from the "public" directory
 app.use(express.static("public"));
-
-// Parse URL-encoded request bodies
 app.use(express.urlencoded({ extended: true }));
 
-// Set the base API endpoint
 const apiUrl =
   "https://cdn.contentful.com/spaces/x2maf5pkzgmb/environments/master/entries?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&content_type=configurableProduct";
 
 const productsData = await fetchJson(apiUrl);
 
-// Route for the homepage
-app.get("/", function (request, response) {
-  console.log(productsData)
-    // Render the 'index' template with the products and the newest product
-    response.render("index", {
-      products: productsData.items,
-    });
+const imageData = productsData.items.map((item) => {
+  const products = item.fields.products.map((productLink) => {
+    const product = productsData.includes.Entry.find(
+      (entry) => entry.sys.id === productLink.sys.id
+    );
+    const imageId = product.fields.primaryImage.sys.id;
+    const imageUrl = productsData.includes.Asset.find(
+      (asset) => asset.sys.id === imageId
+    ).fields.file.url;
+
+    return {
+      title: product.fields.title,
+      imageUrl: `https:${imageUrl}`,
+    };
   });
 
-// Start the server
+  return { products };
+});
+
+app.get("/", function (request, response) {
+  console.log(imageData)
+  response.render("index", {
+    products: productsData.items,
+    items: imageData,
+  });
+});
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, function () {
   console.log(`Application started on http://localhost:${PORT}`);
