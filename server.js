605d0417256ccffdaa5b4c77b1b19d154ecde2f5
lisagjh <lisagjhoorn@outlook.com>
2024-06-17 11:39:20 +0200

    images are finally working - thanks krijn :D

--- server.js
+++ server.js
@@ -18,10 +18,37 @@ const apiProductUrl =
 // Fetch the configurable products from Contentful
 const configurableProductsResponse = await fetchJson(apiUrl);
 const configurableProducts = configurableProductsResponse.items; // Extract the items array from the response
-
+// console.log(configurableProductsResponse)
 // Fetch the simple products from Contentful
 const simpleProductsResponse = await fetchJson(apiProductUrl);
 const simpleProducts = simpleProductsResponse.items; // Extract the items array from the response
+
+
+app.locals.productIDs = {}
+app.locals.productIDsToImages = {}
+app.locals.images = {}
+
+configurableProducts.forEach(function(configurableProduct) {
+  app.locals.productIDs[configurableProduct.sys.id] = []
+  configurableProduct.fields.products.forEach(function(product) {
+    app.locals.productIDs[configurableProduct.sys.id].push(product.sys.id)
+    app.locals.productIDsToImages[product.sys.id] = {}
+  })
+})
+
+configurableProductsResponse.includes.Entry.forEach(function(entry) {
+  app.locals.productIDsToImages[entry.sys.id] = entry.fields.primaryImage.sys.id
+  app.locals.images[entry.fields.primaryImage.sys.id] = {}
+})
+
+configurableProductsResponse.includes.Asset.forEach(function(asset) {
+  app.locals.images[asset.sys.id] = asset.fields.file
+})
+
+console.log(app.locals.productIDs)
+console.log(app.locals.productIDsToImages)
+console.log(app.locals.images)
+
 
 // Define the route for the home page
 app.get("/", function (request, response) {
@@ -41,15 +68,15 @@ app.get("/", function (request, response) {
       // Return an object combining information from both products
       return {
         id: config.sys.id,
+        // TODO: zorg dat je hier toegang hebt tot alle images.. Ooit :)
+        image: app.locals.images[app.locals.productIDsToImages[app.locals.productIDs[config.sys.id][0]]],
         name: config.fields.title,
-        image: simple.fields.primaryImage,
         size: simple.fields.size,
         description: simple.fields.description,
       };
     })
-    .filter((item) => item !== null); // Filter out any null entries
 
-  console.log(combinedProduct); // Log the combined product array to the console
+  // console.log(combinedProduct); // Log the combined product array to the console
   response.render("index", {
     products: combinedProduct, // Pass combinedProduct as 'products' to the EJS template
   });
