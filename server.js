c76777a1903bac0d6b51391203728fbcb85c9ba5
lisagjh <lisagjhoorn@outlook.com>
2024-06-18 22:27:49 +0200

    styles

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set("views", "./views");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());  // Middleware to parse JSON request body
 
 // API URLs to fetch data from Contentful
 const apiUrl =
@@ -45,41 +46,57 @@ configurableProductsResponse.includes.Asset.forEach(function (asset) {
   app.locals.images[asset.sys.id] = asset.fields.file;
 });
 
-// console.log(app.locals.productIDs);
-// console.log(app.locals.productIDsToImages);
-// console.log(app.locals.images)
+// Combine configurable products with their corresponding simple products
+const combinedProduct = configurableProducts.map((config) => {
+  // Find the corresponding simple product
+  const simple = simpleProducts.find(
+    (simple) => simple.sys.id == config.fields.products[0].sys.id
+  );
+
+  // Return an object combining information from both products
+  return {
+    id: config.sys.id,
+    // TODO: zorg dat je hier toegang hebt tot alle images.. Ooit :) niet deze sprint :(
+    image:
+      app.locals.images[
+        app.locals.productIDsToImages[app.locals.productIDs[config.sys.id][0]]
+      ],
+    name: config.fields.title,
+    size: simple.fields.size,
+    description: simple.fields.description,
+  };
+});
 
 // Define the route for the home page
 app.get("/", function (request, response) {
-  // Combine configurable products with their corresponding simple products
-  const combinedProduct = configurableProducts.map((config) => {
-    // Find the corresponding simple product
-    const simple = simpleProducts.find(
-      (simple) => simple.sys.id == config.fields.products[0].sys.id
-    );
-
-    // Return an object combining information from both products
-    return {
-      id: config.sys.id,
-      // TODO: zorg dat je hier toegang hebt tot alle images.. Ooit :)
-      image:
-        app.locals.images[
-          app.locals.productIDsToImages[app.locals.productIDs[config.sys.id][0]]
-        ],
-      name: config.fields.title,
-      size: simple.fields.size,
-      description: simple.fields.description,
-    };
-  });
 
   var lastProduct = combinedProduct[combinedProduct.length - 1];
-
-  console.log("new item:", lastProduct);
 
   response.render("index", {
     products: combinedProduct, // Pass combinedProduct as 'products' to the EJS template
     newProduct: lastProduct,
   });
+});
+
+const cartItems = []
+
+app.post("/cart", (request, response)=> {
+  const productSku = request.body.sku;
+  const product = combinedProduct.find(
+    (product) => product.id === productSku
+  );
+
+  if (product) {
+    const index = cartItems.findIndex((cartItem) => cartItem.id === product.id);
+    if (index === -1) {
+      cartItems.push(product);
+      console.log("product added to cart", product);
+    } else {
+      cartItems.splice(index, 1);
+      console.log("product removed from cart:", productSku)
+    }
+  }
+  response.render('partials/cart', {cartItems: cartItems})
 });
 
 const PORT = process.env.PORT || 8000;
